049d78784950a977109b20a88006eda67aabf00a
ransurf <ransomguest@gmail.com>
2021-12-18 18:10:16 -0800

    refined ui

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,21 +1,69 @@
-import * as React from 'react';
-import Temperature from './components/Temperature';
-import Database from './components/Database';
-import { Button } from '@mui/material';
-import './App.css';
-function App() {
-  return (
-    <div className="App">
-      <h1 className='App-header'>Bridge Watcher</h1>
-      <Temperature temp="25"></Temperature>
-      <Database></Database>
-      <Button onClick={() => {
-        alert('Overload');
-        }}
-        variant="contained">Test Overload
-      </Button>
-    </div>
-  );
+import * as React from "react";
+import Database from "./components/Database";
+import bridgeData from "./bridgeData";
+import { Button, ButtonGroup } from "@mui/material";
+import "./App.css";
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      data: bridgeData,
+      maxLoad: 0,
+    };
+  }
+  setMaxLoad = () => {
+    let load = prompt("Enter max load of the bridge in kg", "0");
+    if (load != null) {
+      this.setState({
+        maxLoad: load,
+      });
+    }
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className>Bridge Watcher 🌉</h1>
+        </header>
+        <div className="App-body">
+          <h2>Vashi Bridge</h2>
+          <h4>Max Load = {this.state.maxLoad} kg</h4>
+          <Button
+            variant="contained"
+            onClick={() => {
+              this.setMaxLoad();
+            }}
+          >
+            Set Load Capacity
+          </Button>
+          <Database data={this.state.data}></Database>
+        </div>
+        <Button
+          variant="contained"
+          onClick={() => {
+            let today = new Date();
+            let date =
+              today.getFullYear() +
+              "-" +
+              (today.getMonth() + 1) +
+              "-" +
+              today.getDate();
+            let time =
+              today.getHours() +
+              ":" +
+              today.getMinutes() +
+              ":" +
+              today.getSeconds();
+            let dateTime = date + " " + time;
+            alert("⚠️ Overload during " + dateTime + " ⚠️");
+          }}
+        >
+          Test Overload
+        </Button>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ my-app/src/bridgeData.js
@@ -0,0 +1,34 @@
+const bridgeData = [
+    {
+        timestamp: "2021-12-18",
+        load: "28.5",
+        compression: "52.9",
+        tension: "182.3"
+    },
+    {
+        timestamp: "2021-12-19",
+        load: "40.5",
+        compression: "18.9",
+        tension: "402.3"
+    },
+    {
+        timestamp: "2021-12-20",
+        load: "125.2",
+        compression: "102.4",
+        tension: "52.7"
+    },
+]
+
+export default bridgeData
+
+
+// function createData(timestamp, currLoad, compression, tension) {
+//     return { timestamp, tension};
+//   }
+  
+//   const rows = [
+//     createData('2021-12-17', 159),
+//     createData('2021-12-18', 237),
+//     createData('2021-12-19', 262),
+  
+//   ];
